ecc308a49861d2442d59230e69684d3ae40fb23d
aamyear <aamyear@gmail.com>
2021-05-21 19:55:58 +0400

    init currency convertor with context

---
+++ README.md
@@ -0,0 +1,1 @@
+# Converting Currency with React Context
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Currency Conversion with React Context</h1>
+    </div>
+  );
+}
+
+export default App;
